cd5c409266a895d0a3b4d404fc0acb43edfc5395
d0by <dobbyvoe@gmail.com>
2021-11-27 00:15:53 +0100

    Fix

--- src/main/java/eu/decentsoftware/holograms/api/holograms/HologramLine.java
+++ src/main/java/eu/decentsoftware/holograms/api/holograms/HologramLine.java
@@ -269,11 +269,10 @@ public class HologramLine extends HologramObject {
             if (!hasFlag(EnumFlag.DISABLE_PLACEHOLDERS)) {
                 string = PAPI.setPlaceholders(player, string);
             }
-            playerTextMap.put(uuid, string
-                    .replace("{player}", player.getName())
-                    .replace("{page}", String.valueOf(hasParent() ? parent.getIndex() : 1))
-                    .replace("{pages}", String.valueOf(hasParent() ? parent.getParent().size() : 1))
-            );
+            string = string.replace("{player}", player.getName())
+                    .replace("{page}", String.valueOf(hasParent() ? parent.getIndex() + 1 : 1))
+                    .replace("{pages}", String.valueOf(hasParent() ? parent.getParent().size() : 1));
+            playerTextMap.put(uuid, string);
         }
         // Replace Animations
         if (!hasFlag(EnumFlag.DISABLE_ANIMATIONS)) {
