be915e9b69832cb8412391c74c236cbc48f8a694
Andre601 <11576465+Andre601@users.noreply.github.com>
2022-05-06 18:56:16 +0200

    Be gone unused import!

--- src/main/java/eu/decentsoftware/holograms/api/utils/color/IridiumColorAPI.java
+++ src/main/java/eu/decentsoftware/holograms/api/utils/color/IridiumColorAPI.java
@@ -12,7 +12,6 @@ import net.md_5.bungee.api.ChatColor;
 import javax.annotation.Nonnull;
 import java.awt.*;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
