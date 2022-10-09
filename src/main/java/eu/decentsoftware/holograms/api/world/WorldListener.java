74ef6f7879dfe0ffa352f9b12b2535949d2495de
d0by <dobbyvoe@gmail.com>
2022-10-09 20:50:19 +0200

    World load/unload handler made async

--- src/main/java/eu/decentsoftware/holograms/api/world/WorldListener.java
+++ src/main/java/eu/decentsoftware/holograms/api/world/WorldListener.java
@@ -6,6 +6,8 @@ import eu.decentsoftware.holograms.api.holograms.DisableCause;
 import eu.decentsoftware.holograms.api.holograms.Hologram;
 import eu.decentsoftware.holograms.api.holograms.HologramManager;
 import eu.decentsoftware.holograms.api.utils.exception.LocationParseException;
+import eu.decentsoftware.holograms.api.utils.scheduler.S;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.WorldLoadEvent;
@@ -17,33 +19,39 @@ public class WorldListener implements Listener {
 
     @EventHandler
     public void onWorldUnload(WorldUnloadEvent event) {
-        DH.getHologramManager().getHolograms().stream()
+        HologramManager hm = DH.getHologramManager();
+        World world = event.getWorld();
+
+        S.async(() -> hm.getHolograms().stream()
                 .filter(Hologram::isEnabled)
-                .filter(hologram -> hologram.getLocation().getWorld().getName().equals(event.getWorld().getName()))
-                .forEach(hologram -> hologram.disable(DisableCause.WORLD_UNLOAD));
+                .filter(hologram -> hologram.getLocation().getWorld().equals(world))
+                .forEach(hologram -> hologram.disable(DisableCause.WORLD_UNLOAD)));
     }
 
     @EventHandler
     public void onWorldLoad(WorldLoadEvent event) {
         HologramManager hm = DH.getHologramManager();
-        String worldName = event.getWorld().getName();
-        if (hm.getToLoad().containsKey(worldName)) {
-            hm.getToLoad().get(worldName).forEach((fileName) -> {
-                try {
-                    Hologram hologram = Hologram.fromFile(fileName);
-                    if (hologram != null && hologram.isEnabled()) {
-                        hologram.showAll();
-                        hologram.realignLines();
-                        hm.registerHologram(hologram);
+        World world = event.getWorld();
+
+        S.async(() -> {
+            if (hm.getToLoad().containsKey(world.getName())) {
+                hm.getToLoad().get(world.getName()).forEach((fileName) -> {
+                    try {
+                        Hologram hologram = Hologram.fromFile(fileName);
+                        if (hologram != null && hologram.isEnabled()) {
+                            hologram.showAll();
+                            hologram.realignLines();
+                            hm.registerHologram(hologram);
+                        }
+                    } catch (LocationParseException ignored) {
                     }
-                } catch (LocationParseException ignored) {
-                }
-            });
-        }
-        hm.getHolograms().stream()
-                .filter(hologram -> !hologram.isEnabled())
-                .filter(hologram -> hologram.getLocation().getWorld().getName().equals(worldName))
-                .filter(hologram -> hologram.getDisableCause().equals(DisableCause.WORLD_UNLOAD))
-                .forEach(Hologram::enable);
+                });
+            }
+            hm.getHolograms().stream()
+                    .filter(hologram -> !hologram.isEnabled())
+                    .filter(hologram -> hologram.getLocation().getWorld().equals(world))
+                    .filter(hologram -> hologram.getDisableCause().equals(DisableCause.WORLD_UNLOAD))
+                    .forEach(Hologram::enable);
+        });
     }
 }
