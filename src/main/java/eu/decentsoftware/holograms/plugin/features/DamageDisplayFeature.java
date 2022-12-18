d9b00abf44d50bcbf50ef805a284f2e5b15de159
d0by <dobbyvoe@gmail.com>
2022-12-18 19:35:59 +0100

    Critical damage appearance in DamageDisplayFeature

--- src/main/java/eu/decentsoftware/holograms/plugin/features/DamageDisplayFeature.java
+++ src/main/java/eu/decentsoftware/holograms/plugin/features/DamageDisplayFeature.java
@@ -6,7 +6,9 @@ import eu.decentsoftware.holograms.api.Settings;
 import eu.decentsoftware.holograms.api.features.AbstractFeature;
 import eu.decentsoftware.holograms.api.utils.config.FileConfig;
 import eu.decentsoftware.holograms.api.utils.location.LocationUtils;
+import lombok.NonNull;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -14,14 +16,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffectType;
 
 public class DamageDisplayFeature extends AbstractFeature implements Listener {
 
 	private static final DecentHolograms PLUGIN = DecentHologramsAPI.get();
 	private int duration = 40;
-	private String appearance = "&c+ {damage}";
+	private String appearance = "&c{damage}";
+	private String criticalAppearance = "&4&lCrit!&4 {damage}";
 	private boolean displayForPlayers = true;
 	private boolean displayForMobs = true;
 	private boolean zeroDamage = false;
@@ -40,6 +45,7 @@ public class DamageDisplayFeature extends AbstractFeature implements Listener {
 		enabled = config.getBoolean("damage-display.enabled", enabled);
 		duration = config.getInt("damage-display.duration", duration);
 		appearance = config.getString("damage-display.appearance", appearance);
+		criticalAppearance = config.getString("damage-display.critical-appearance", criticalAppearance);
 
 		heightOffset = config.getDouble("healing-display.height", heightOffset);
 
@@ -102,8 +108,41 @@ public class DamageDisplayFeature extends AbstractFeature implements Listener {
 		}
 
 		Location location = LocationUtils.randomizeLocation(entity.getLocation().clone().add(0, 1 + heightOffset, 0));
+		String appearance;
+		Entity damager = (e instanceof EntityDamageByEntityEvent) ? ((EntityDamageByEntityEvent) e).getDamager() : null;
+		if (damager instanceof Player && isCritical((Player) damager)) {
+			appearance = this.criticalAppearance;
+		} else {
+			appearance = this.appearance;
+		}
 		String text = appearance.replace("{damage}", FeatureCommons.formatNumber(damage));
 		PLUGIN.getHologramManager().spawnTemporaryHologramLine(location, text, duration);
+	}
+
+	/**
+	 * Check if the damage dealt, by the given player is critical.
+	 *
+	 * @param player The player.
+	 * @return True if the damage is critical, false otherwise.
+	 */
+	private boolean isCritical(@NonNull Player player) {
+		if (player.getFallDistance() <= 0.0F ||
+				player.isOnGround() ||
+				player.isInsideVehicle() ||
+				player.hasPotionEffect(PotionEffectType.BLINDNESS) ||
+				player.getLocation().getBlock().getType() == Material.LADDER ||
+				player.getLocation().getBlock().getType() == Material.VINE
+		) {
+			return false;
+		}
+		try {
+			// Slow Falling is not in all versions
+			if (player.hasPotionEffect(PotionEffectType.SLOW_FALLING)) {
+				return false;
+			}
+		} catch (Exception ignored) {
+		}
+		return true;
 	}
 
 }
