f6fde1c2d0d1fa56a33caa93f8795d73be6a84b9
d0by <dobbyvoe@gmail.com>
2023-01-24 18:14:25 +0100

    Placeholder for text inside animations

--- src/main/java/eu/decentsoftware/holograms/api/animations/custom/CustomTextAnimation.java
+++ src/main/java/eu/decentsoftware/holograms/api/animations/custom/CustomTextAnimation.java
@@ -11,40 +11,42 @@ import java.util.List;
 
 public class CustomTextAnimation extends TextAnimation {
 
-    private static final DecentHolograms DECENT_HOLOGRAMS = DecentHologramsAPI.get();
-    private final @NonNull List<String> steps;
+	private static final DecentHolograms DECENT_HOLOGRAMS = DecentHologramsAPI.get();
+	private final @NonNull List<String> steps;
 
-    public CustomTextAnimation(@NonNull String name, int speed, int pause, @NonNull List<String> steps) {
-        super(name, speed, pause);
-        this.steps = steps;
-    }
+	public CustomTextAnimation(@NonNull String name, int speed, int pause, @NonNull List<String> steps) {
+		super(name, speed, pause);
+		this.steps = steps;
+	}
 
-    public CustomTextAnimation(@NonNull String name, int speed, int pause, @NonNull List<String> steps, String... aliases) {
-        super(name, speed, pause, aliases);
-        this.steps = steps;
-    }
+	public CustomTextAnimation(@NonNull String name, int speed, int pause, @NonNull List<String> steps, String... aliases) {
+		super(name, speed, pause, aliases);
+		this.steps = steps;
+	}
 
-    @Override
-    public String animate(@NonNull String string, long step, String... args) {
-        int currentStep = getCurrentStep(step, steps.size() - 1);
-        return steps.get(currentStep);
-    }
+	@Override
+	public String animate(@NonNull String string, long step, String... args) {
+		int currentStepIndex = getCurrentStep(step, steps.size() - 1);
+		String currentStep = steps.get(currentStepIndex);
+		currentStep = currentStep.replace("{text}", string);
+		return currentStep;
+	}
 
-    public static CustomTextAnimation fromFile(@NonNull String fileName) {
-        FileConfig config = new FileConfig(DECENT_HOLOGRAMS.getPlugin(), "animations/" + fileName);
+	public static CustomTextAnimation fromFile(@NonNull String fileName) {
+		FileConfig config = new FileConfig(DECENT_HOLOGRAMS.getPlugin(), "animations/" + fileName);
 
-        // Parse animation name
-        String name;
-        if (fileName.toLowerCase().startsWith("animation_") && fileName.length() > "animation_".length()) {
-            name = fileName.substring("animation_".length(), fileName.length() - 4);
-        } else {
-            name = fileName.substring(0, fileName.length() - 4);
-        }
+		// Parse animation name
+		String name;
+		if (fileName.toLowerCase().startsWith("animation_") && fileName.length() > "animation_".length()) {
+			name = fileName.substring("animation_".length(), fileName.length() - 4);
+		} else {
+			name = fileName.substring(0, fileName.length() - 4);
+		}
 
-        int speed = config.isInt("speed") ? config.getInt("speed") : 1;
-        int pause = config.isInt("pause") ? config.getInt("pause") : 1;
-        List<String> steps = config.isList("steps") ? config.getStringList("steps") : new ArrayList<>();
-        return new CustomTextAnimation(name, speed, pause, steps);
-    }
+		int speed = config.isInt("speed") ? config.getInt("speed") : 1;
+		int pause = config.isInt("pause") ? config.getInt("pause") : 1;
+		List<String> steps = config.isList("steps") ? config.getStringList("steps") : new ArrayList<>();
+		return new CustomTextAnimation(name, speed, pause, steps);
+	}
 
 }
