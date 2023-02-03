d5b24cca2254cb6713c0107d074b1c073dd79f2e
d0by <dobbyvoe@gmail.com>
2023-02-03 14:21:32 +0100

    Fix skull textures

--- src/main/java/eu/decentsoftware/holograms/api/utils/items/SkullUtils.java
+++ src/main/java/eu/decentsoftware/holograms/api/utils/items/SkullUtils.java
@@ -11,12 +11,15 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.jetbrains.annotations.Nullable;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Base64;
+import java.net.URL;
 import java.util.Collection;
 import java.util.UUID;
 
@@ -30,185 +33,223 @@ import java.util.UUID;
 @UtilityClass
 public final class SkullUtils {
 
-    private static Field PROFILE_FIELD;
-    private static Method SET_PROFILE_METHOD;
-    private static boolean INITIALIZED = false;
+	private static Field PROFILE_FIELD;
+	private static Method SET_PROFILE_METHOD;
+	private static boolean INITIALIZED = false;
 
-    /**
-     * Get the Base64 texture of the given skull ItemStack.
-     *
-     * @param itemStack The ItemStack.
-     * @return The skull texture. (Base64)
-     */
-    @Nullable
-    public static String getSkullTexture(@NonNull ItemStack itemStack) {
-        try {
-            ItemMeta meta = itemStack.getItemMeta();
-            if (!(meta instanceof SkullMeta)) {
-                return null;
-            }
+	/**
+	 * Get the Base64 texture of the given skull ItemStack.
+	 *
+	 * @param itemStack The ItemStack.
+	 * @return The skull texture. (Base64)
+	 */
+	@Nullable
+	public static String getSkullTexture(@NonNull ItemStack itemStack) {
+		try {
+			ItemMeta meta = itemStack.getItemMeta();
+			if (!(meta instanceof SkullMeta)) {
+				return null;
+			}
 
-            if (PROFILE_FIELD == null) {
-                PROFILE_FIELD = meta.getClass().getDeclaredField("profile");
-                PROFILE_FIELD.setAccessible(true);
-            }
+			if (PROFILE_FIELD == null) {
+				PROFILE_FIELD = meta.getClass().getDeclaredField("profile");
+				PROFILE_FIELD.setAccessible(true);
+			}
 
-            GameProfile profile = (GameProfile) PROFILE_FIELD.get(meta);
-            if (profile == null) {
-                return null;
-            }
+			GameProfile profile = (GameProfile) PROFILE_FIELD.get(meta);
+			if (profile == null) {
+				return null;
+			}
 
-            PropertyMap properties = profile.getProperties();
-            Collection<Property> property = properties.get("textures");
-            if (property != null && !property.isEmpty()) {
-                return property.stream().findFirst().get().getValue();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+			PropertyMap properties = profile.getProperties();
+			Collection<Property> property = properties.get("textures");
+			if (property != null && !property.isEmpty()) {
+				return property.stream().findFirst().get().getValue();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-    /**
-     * Get the Base64 texture of the given skull ItemStack.
-     *
-     * @param itemStack The ItemStack.
-     * @return The skull texture. (Base64)
-     * @deprecated Use {@link #getSkullTexture(ItemStack)} instead.
-     */
-    @Deprecated
-    @Nullable
-    public static String getTexture(@NonNull ItemStack itemStack) {
-        return getSkullTexture(itemStack);
-    }
+	/**
+	 * Get the Base64 texture of the given skull ItemStack.
+	 *
+	 * @param itemStack The ItemStack.
+	 * @return The skull texture. (Base64)
+	 * @deprecated Use {@link #getSkullTexture(ItemStack)} instead.
+	 */
+	@Deprecated
+	@Nullable
+	public static String getTexture(@NonNull ItemStack itemStack) {
+		return getSkullTexture(itemStack);
+	}
 
-    /**
-     * Set the Base64 texture of the given skull ItemStack.
-     *
-     * @param itemStack The ItemStack.
-     * @param texture   The new skull texture (Base64).
-     */
-    public static void setSkullTexture(@NonNull ItemStack itemStack, @NonNull String texture) {
-        try {
-            ItemMeta meta = itemStack.getItemMeta();
-            if (meta instanceof SkullMeta) {
-                GameProfile profile = new GameProfile(UUID.randomUUID(), null);
-                Property property = new Property("textures", texture);
+	/**
+	 * Set the Base64 texture of the given skull ItemStack.
+	 *
+	 * @param itemStack The ItemStack.
+	 * @param texture   The new skull texture (Base64).
+	 */
+	public static void setSkullTexture(@NonNull ItemStack itemStack, @NonNull String texture) {
+		try {
+			ItemMeta meta = itemStack.getItemMeta();
+			if (meta instanceof SkullMeta) {
+				GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+				Property property = new Property("textures", texture);
 
-                PropertyMap properties = profile.getProperties();
-                properties.put("textures", property);
+				PropertyMap properties = profile.getProperties();
+				properties.put("textures", property);
 
-                if (SET_PROFILE_METHOD == null && !INITIALIZED) {
-                    try {
-                        // This method only exists in versions 1.16 and up. For older versions we use reflection
-                        // to set the profile field directly.
-                        SET_PROFILE_METHOD = meta.getClass().getDeclaredMethod("setProfile", GameProfile.class);
-                        SET_PROFILE_METHOD.setAccessible(true);
-                    } catch (NoSuchMethodException e) {
-                        // Server is running an older version.
-                    }
-                    INITIALIZED = true;
-                }
+				if (SET_PROFILE_METHOD == null && !INITIALIZED) {
+					try {
+						// This method only exists in versions 1.16 and up. For older versions we use reflection
+						// to set the profile field directly.
+						SET_PROFILE_METHOD = meta.getClass().getDeclaredMethod("setProfile", GameProfile.class);
+						SET_PROFILE_METHOD.setAccessible(true);
+					} catch (NoSuchMethodException e) {
+						// Server is running an older version.
+					}
+					INITIALIZED = true;
+				}
 
-                if (SET_PROFILE_METHOD != null) {
-                    SET_PROFILE_METHOD.invoke(meta, profile);
-                } else {
-                    if (PROFILE_FIELD == null) {
-                        PROFILE_FIELD = meta.getClass().getDeclaredField("profile");
-                        PROFILE_FIELD.setAccessible(true);
-                    }
-                    PROFILE_FIELD.set(meta, profile);
-                }
-            }
-            itemStack.setItemMeta(meta);
+				if (SET_PROFILE_METHOD != null) {
+					SET_PROFILE_METHOD.invoke(meta, profile);
+				} else {
+					if (PROFILE_FIELD == null) {
+						PROFILE_FIELD = meta.getClass().getDeclaredField("profile");
+						PROFILE_FIELD.setAccessible(true);
+					}
+					PROFILE_FIELD.set(meta, profile);
+				}
+			}
+			itemStack.setItemMeta(meta);
 
-            if (Version.before(13)) {
-                // noinspection deprecation
-                itemStack.setDurability((short) SkullType.PLAYER.ordinal());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+			if (Version.before(13)) {
+				// noinspection deprecation
+				itemStack.setDurability((short) SkullType.PLAYER.ordinal());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * Set the texture of the given skull ItemStack from URL.
-     *
-     * @param itemStack The ItemStack.
-     * @param url       The URL to get the texture from.
-     * @since 2.7.5
-     */
-    public static void setSkullTextureFromURL(@NonNull ItemStack itemStack, @NonNull String url) {
-        setSkullTexture(itemStack, getTextureFromURL(url));
-    }
+	/**
+	 * Get the owner of the given skull ItemStack.
+	 *
+	 * @param itemStack The ItemStack.
+	 * @return The skull owner.
+	 * @since 2.7.5
+	 */
+	@Nullable
+	public static String getSkullOwner(@NonNull ItemStack itemStack) {
+		ItemMeta meta = itemStack.getItemMeta();
+		if (meta instanceof SkullMeta) {
+			return ((SkullMeta) meta).getOwner();
+		}
+		return null;
+	}
 
-    /**
-     * Get the owner of the given skull ItemStack.
-     *
-     * @param itemStack The ItemStack.
-     * @return The skull owner.
-     * @since 2.7.5
-     */
-    @Nullable
-    public static String getSkullOwner(@NonNull ItemStack itemStack) {
-        ItemMeta meta = itemStack.getItemMeta();
-        if (meta instanceof SkullMeta) {
-            return ((SkullMeta) meta).getOwner();
-        }
-        return null;
-    }
+	/**
+	 * Set the owner of the given skull ItemStack.
+	 *
+	 * @param itemStack The ItemStack.
+	 * @param owner     The new skull owner.
+	 * @since 2.7.5
+	 */
+	public static void setSkullOwner(@NonNull ItemStack itemStack, @NonNull String owner) {
+		ItemMeta meta = itemStack.getItemMeta();
+		if (meta instanceof SkullMeta) {
+			((SkullMeta) meta).setOwner(owner);
 
-    /**
-     * Set the owner of the given skull ItemStack.
-     *
-     * @param itemStack The ItemStack.
-     * @param owner     The new skull owner.
-     * @since 2.7.5
-     */
-    public static void setSkullOwner(@NonNull ItemStack itemStack, @NonNull String owner) {
-        ItemMeta meta = itemStack.getItemMeta();
-        if (meta instanceof SkullMeta) {
-            ((SkullMeta) meta).setOwner(owner);
+			itemStack.setItemMeta(meta);
 
-            itemStack.setItemMeta(meta);
+			if (Version.before(13)) {
+				// noinspection deprecation
+				itemStack.setDurability((short) SkullType.PLAYER.ordinal());
+			}
+		}
+	}
 
-            if (Version.before(13)) {
-                // noinspection deprecation
-                itemStack.setDurability((short) SkullType.PLAYER.ordinal());
-            }
-        }
-    }
+	/**
+	 * Get a Base64 texture from URL by player username.
+	 *
+	 * @param username The player username.
+	 * @return The Base64 or null if the URL is invalid.
+	 * @since 2.7.10
+	 */
+	@Nullable
+	public static String getTextureFromURLByPlayerName(String username) {
+		final String uuid = getPlayerUUID(username);
+		if (uuid == null) {
+			return null;
+		}
+		final String url = "https://api.minetools.eu/profile/" + uuid;
+		try {
+			JSONParser jsonParser = new JSONParser();
+			String userData = readUrl(url);
+			Object parsedData = jsonParser.parse(userData);
 
-    /**
-     * Get a Base64 skull texture from URL.
-     *
-     * @param url The URL.
-     * @return The Base64 texture or null if the URL is invalid.
-     * @since 2.7.5
-     */
-    @Nullable
-    public static String getTextureFromURL(String url) {
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            return null;
-        }
-        String toEncode = "{\"textures\":{\"SKIN\":{\"url\":\"" + uri.toString() + "\"}}}";
-        return Base64.getEncoder().encodeToString(toEncode.getBytes());
-    }
+			JSONObject jsonData = (JSONObject) parsedData;
+			JSONObject decoded = (JSONObject) jsonData.get("raw");
+			JSONArray textures = (JSONArray) decoded.get("properties");
+			JSONObject data = (JSONObject) textures.get(0);
 
-    /**
-     * Get a Base64 skull texture from URL by player name.
-     *
-     * @param playerName The player name.
-     * @return The Base64 texture or null if the URL is invalid.
-     * @since 2.7.10
-     */
-    @Nullable
-    public static String getTextureFromURLByPlayerName(String playerName) {
-        return getTextureFromURL("https://minotar.net/helm/" + playerName + "/16.png");
-    }
+			return data.get("value").toString();
+		} catch (Exception ignored) {
+		}
+		return null;
+	}
+
+	/**
+	 * Fetch the UUID of a player from Minetools's API.
+	 *
+	 * @param playerName The player name.
+	 * @return
+	 */
+	@Nullable
+	public static String getPlayerUUID(String playerName) {
+		try {
+			String url = "https://api.minetools.eu/uuid/" + playerName;
+			JSONParser jsonParser = new JSONParser();
+			String userData = readUrl(url);
+			Object parsedData = jsonParser.parse(userData);
+
+			JSONObject jsonData = (JSONObject) parsedData;
+
+			if (jsonData.get("id") != null) {
+				return jsonData.get("id").toString();
+			}
+		} catch (Exception ignored) {
+		}
+		return null;
+	}
+
+	/**
+	 * Read the content of a URL.
+	 *
+	 * @param urlString The URL.
+	 * @return The content.
+	 * @throws Exception
+	 */
+	@NonNull
+	private static String readUrl(String urlString) throws Exception {
+		BufferedReader reader = null;
+		try {
+			URL url = new URL(urlString);
+			reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			StringBuilder builder = new StringBuilder();
+			int read;
+			char[] chars = new char[1024];
+			while ((read = reader.read(chars)) != -1) {
+				builder.append(chars, 0, read);
+			}
+			return builder.toString();
+		} finally {
+			if (reader != null) {
+				reader.close();
+			}
+		}
+	}
 
 }
