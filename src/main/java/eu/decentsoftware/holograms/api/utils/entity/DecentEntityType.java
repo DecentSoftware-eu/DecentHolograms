f208afed8302e99b5fa54881975baa8dd8718640
tplansky <tomas.plansky@generalbytes.com>
2024-06-03 22:13:00 +0200

    Rework entity types using XEntityType

--- src/main/java/eu/decentsoftware/holograms/api/utils/entity/DecentEntityType.java
+++ src/main/java/eu/decentsoftware/holograms/api/utils/entity/DecentEntityType.java
@@ -1,12 +1,12 @@
 package eu.decentsoftware.holograms.api.utils.entity;
 
+import com.cryptomorin.xseries.XEntityType;
 import com.google.common.collect.Sets;
 import eu.decentsoftware.holograms.api.utils.Common;
 import lombok.experimental.UtilityClass;
 import org.bukkit.entity.EntityType;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
@@ -18,82 +18,77 @@ import java.util.stream.Collectors;
 @UtilityClass
 public final class DecentEntityType {
 
-    private static final Map<String, EntityType> ENTITY_TYPE_ALIASES = new HashMap<>();
-    private static final Set<EntityType> ENTITY_TYPE_BLACKLIST;
-    private static final Set<EntityType> ENTITY_TYPES;
+    private static final Map<String, XEntityType> ENTITY_TYPE_ALIASES = new HashMap<>();
+    private static final Set<XEntityType> ENTITY_TYPE_BLACKLIST;
+    private static final Set<XEntityType> ENTITY_TYPES;
 
     static {
-        ENTITY_TYPES = EnumSet.allOf(EntityType.class);
-
+        ENTITY_TYPES = EnumSet.allOf(XEntityType.class);
         ENTITY_TYPE_BLACKLIST = Sets.newHashSet(
-                        "ARMOR_STAND",
-                        "PRIMED_TNT",
-                        "AREA_EFFECT_CLOUD",
-                        "FISHING_HOOK",
-                        "GIANT",
-                        "EVOKER_FANGS",
-                        "EXPERIENCE_ORB",
-                        "LEASH_HITCH",
-                        "DROPPED_ITEM",
-                        "ITEM_FRAME",
-                        "GLOW_ITEM_FRAME",
-                        "THROWN_EXP_BOTTLE",
-                        "SPLASH_POTION",
-                        "SPECTRAL_ARROW",
-                        "LLAMA_SPIT",
-                        "MARKER",
-                        "LIGHTNING",
-                        "TRIDENT",
-                        "PAINTING",
-                        "PLAYER",
-                        "MINECART",
-                        "MINECART_CHEST",
-                        "MINECART_COMMAND",
-                        "MINECART_FURNACE",
-                        "MINECART_HOPPER",
-                        "MINECART_MOB_SPAWNER",
-                        "MINECART_TNT",
-                        "COMPLEX_PART",
-                        "WEATHER",
-                        "TIPPED_ARROW",
-                        "UNKNOWN"
-                ).stream()
-                .map(name -> {
-                    try {
-                        return EntityType.valueOf(name);
-                    } catch (IllegalArgumentException e) {
-                        return null;
-                    }
-                })
-                .filter(Objects::nonNull)
-                .collect(Collectors.toSet());
+                XEntityType.AREA_EFFECT_CLOUD,
+                XEntityType.BLOCK_DISPLAY,
+                XEntityType.EVOKER_FANGS,
+                XEntityType.FALLING_BLOCK,
+                XEntityType.FISHING_BOBBER,
+                XEntityType.GLOW_ITEM_FRAME,
+                XEntityType.INTERACTION,
+                XEntityType.ITEM,
+                XEntityType.ITEM_DISPLAY,
+                XEntityType.ITEM_FRAME,
+                XEntityType.LEASH_KNOT,
+                XEntityType.LIGHTNING_BOLT,
+                XEntityType.MARKER,
+                XEntityType.PAINTING,
+                XEntityType.PLAYER,
+                XEntityType.TEXT_DISPLAY,
+                XEntityType.TNT,
+                XEntityType.UNKNOWN
+        );
 
         ENTITY_TYPES.removeIf(ENTITY_TYPE_BLACKLIST::contains);
 
-        for (EntityType entityType : ENTITY_TYPES) {
-            ENTITY_TYPE_ALIASES.put(Common.removeSpacingChars(entityType.name()).toLowerCase(), entityType);
+        for (XEntityType xEntityType : ENTITY_TYPES) {
+            String name = getBukkitEntityTypeName(xEntityType);
+            if (name != null) {
+                ENTITY_TYPE_ALIASES.put(Common.removeSpacingChars(name).toLowerCase(), xEntityType);
+            }
         }
-    }
-
-    public static List<EntityType> getAllowedEntityTypes() {
-        return new ArrayList<>(ENTITY_TYPES);
     }
 
     public static List<String> getAllowedEntityTypeNames() {
         return ENTITY_TYPES.stream()
-                .map(EntityType::name)
+                .map(DecentEntityType::getBukkitEntityTypeName)
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
     }
 
+    private static String getBukkitEntityTypeName(XEntityType xEntityType) {
+        EntityType entityType = xEntityType.get();
+        if (entityType != null) {
+            return entityType.name();
+        }
+        return null;
+    }
+
     public static boolean isAllowed(EntityType entityType) {
+        return isAllowed(XEntityType.of(entityType));
+    }
+
+    public static boolean isAllowed(XEntityType entityType) {
         return !ENTITY_TYPE_BLACKLIST.contains(entityType);
     }
 
     @Nullable
     public static EntityType parseEntityType(String string) {
-        EntityType entityType = ENTITY_TYPE_ALIASES.get(Common.removeSpacingChars(string).toLowerCase());
+        if (string == null) {
+            return null;
+        }
+        XEntityType entityType = ENTITY_TYPE_ALIASES.get(Common.removeSpacingChars(string).toLowerCase());
+        if (entityType == null) {
+            entityType = XEntityType.of(string);
+        }
         if (entityType != null && isAllowed(entityType)) {
-            return entityType;
+            return entityType.get();
         }
         return null;
     }
