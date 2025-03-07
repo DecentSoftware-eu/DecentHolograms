2b12f3d97faccc7ad01d3063d5e917fb8c4e2eec
d0by1 <dobbyvoe@gmail.com>
2025-03-08 00:44:59 +0100

    Implement NMS for v1_12_R1

---
+++ nms/nms-v1_12_R1/src/main/java/eu/decentsoftware/holograms/nms/v1_12_R1/TextHologramRenderer.java
@@ -0,0 +1,65 @@
+package eu.decentsoftware.holograms.nms.v1_12_R1;
+
+import eu.decentsoftware.holograms.nms.api.renderer.NmsTextHologramRenderer;
+import eu.decentsoftware.holograms.shared.DecentPosition;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+class TextHologramRenderer implements NmsTextHologramRenderer {
+
+    private final int armorStandEntityId;
+
+    TextHologramRenderer(EntityIdGenerator entityIdGenerator) {
+        this.armorStandEntityId = entityIdGenerator.getFreeEntityId();
+    }
+
+    @Override
+    public void display(Player player, DecentPosition position, String content) {
+        EntityPacketsBuilder.create()
+                .withSpawnEntityLiving(armorStandEntityId, EntityType.ARMOR_STAND, offsetPosition(position))
+                .withEntityMetadata(armorStandEntityId, EntityMetadataBuilder.create()
+                        .withInvisible()
+                        .withNoGravity()
+                        .withArmorStandProperties(true, true)
+                        .withCustomName(content)
+                        .toWatchableObjects())
+                .sendTo(player);
+    }
+
+    @Override
+    public void updateContent(Player player, DecentPosition position, String content) {
+        EntityPacketsBuilder.create()
+                .withEntityMetadata(armorStandEntityId, EntityMetadataBuilder.create()
+                        .withCustomName(content)
+                        .toWatchableObjects())
+                .sendTo(player);
+    }
+
+    @Override
+    public void move(Player player, DecentPosition position) {
+        EntityPacketsBuilder.create()
+                .withTeleportEntity(armorStandEntityId, offsetPosition(position))
+                .sendTo(player);
+    }
+
+    @Override
+    public void hide(Player player) {
+        EntityPacketsBuilder.create()
+                .withRemoveEntity(armorStandEntityId)
+                .sendTo(player);
+    }
+
+    @Override
+    public double getHeight(String content) {
+        return 0.25d;
+    }
+
+    @Override
+    public int[] getEntityIds() {
+        return new int[]{armorStandEntityId};
+    }
+
+    private DecentPosition offsetPosition(DecentPosition position) {
+        return position.subtractY(0.5);
+    }
+}
