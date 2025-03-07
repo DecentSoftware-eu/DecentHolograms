92a0b99db9c47f36314b78b7767925d808f47db5
d0by1 <dobbyvoe@gmail.com>
2025-03-07 20:16:45 +0100

    Implement NMS for v1_9_R1

---
+++ nms/nms-v1_9_R1/src/main/java/eu/decentsoftware/holograms/nms/v1_9_R1/EntityMetadataBuilder.java
@@ -0,0 +1,85 @@
+package eu.decentsoftware.holograms.nms.v1_9_R1;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Strings;
+import net.minecraft.server.v1_9_R1.DataWatcher;
+import org.bukkit.craftbukkit.v1_9_R1.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class EntityMetadataBuilder {
+
+    private final List<DataWatcher.Item<?>> watchableObjects;
+
+    private EntityMetadataBuilder() {
+        this.watchableObjects = new ArrayList<>();
+    }
+
+    List<DataWatcher.Item<?>> toWatchableObjects() {
+        return this.watchableObjects;
+    }
+
+    EntityMetadataBuilder withInvisible() {
+        /*
+         * Entity Properties:
+         * 0x01 - On Fire
+         * 0x02 - Crouched
+         * 0x08 - Sprinting
+         * 0x10 - Eating/Drinking/Blocking
+         * 0x20 - Invisible
+         * 0x40 - Has glowing effect
+         * 0x80 - If flying with an elytra
+         */
+
+        this.watchableObjects.add(EntityMetadataType.ENTITY_PROPERTIES.construct((byte) 0x20));
+        return this;
+    }
+
+    EntityMetadataBuilder withArmorStandProperties(boolean small, boolean marker) {
+        /*
+         * Armor Stand Properties:
+         * 0x01 - Small
+         * 0x02 - Unused
+         * 0x04 - Has Arms
+         * 0x08 - Remove Baseplate
+         * 0x10 - Marker (Zero bounding box)
+         */
+
+        byte data = 0x08; // Always Remove Baseplate
+        if (small) {
+            data |= 0x01;
+        }
+        if (marker) {
+            data |= 0x10;
+        }
+
+        this.watchableObjects.add(EntityMetadataType.ARMOR_STAND_PROPERTIES.construct(data));
+        return this;
+    }
+
+    EntityMetadataBuilder withCustomName(String customName) {
+        this.watchableObjects.add(EntityMetadataType.ENTITY_CUSTOM_NAME.construct(customName));
+        boolean visible = !Strings.isNullOrEmpty(customName);
+        this.watchableObjects.add(EntityMetadataType.ENTITY_CUSTOM_NAME_VISIBLE.construct(visible));
+        return this;
+    }
+
+    EntityMetadataBuilder withItemStack(ItemStack itemStack) {
+        Optional<net.minecraft.server.v1_9_R1.ItemStack> itemStackOptional = Optional.fromNullable(
+                CraftItemStack.asNMSCopy(itemStack));
+        this.watchableObjects.add(EntityMetadataType.ITEM_STACK.construct(itemStackOptional));
+        return this;
+    }
+
+    EntityMetadataBuilder withNoGravity() {
+        this.watchableObjects.add(EntityMetadataType.ENTITY_HAS_NO_GRAVITY.construct(true));
+        return this;
+    }
+
+    static EntityMetadataBuilder create() {
+        return new EntityMetadataBuilder();
+    }
+
+}
---
+++ nms/nms-v1_9_R1/src/main/java/eu/decentsoftware/holograms/nms/v1_9_R1/EntityMetadataType.java
@@ -0,0 +1,44 @@
+package eu.decentsoftware.holograms.nms.v1_9_R1;
+
+import com.google.common.base.Optional;
+import eu.decentsoftware.holograms.shared.reflect.ReflectUtil;
+import net.minecraft.server.v1_9_R1.DataWatcher;
+import net.minecraft.server.v1_9_R1.DataWatcherObject;
+import net.minecraft.server.v1_9_R1.Entity;
+import net.minecraft.server.v1_9_R1.EntityArmorStand;
+import net.minecraft.server.v1_9_R1.EntityItem;
+import net.minecraft.server.v1_9_R1.ItemStack;
+
+class EntityMetadataType<T> {
+
+    private static final DataWatcherObject<Byte> ENTITY_PROPERTIES_OBJECT
+            = ReflectUtil.getFieldValue(Entity.class, "ax");
+    private static final DataWatcherObject<String> ENTITY_CUSTOM_NAME_OBJECT
+            = ReflectUtil.getFieldValue(Entity.class, "az");
+    private static final DataWatcherObject<Boolean> ENTITY_CUSTOM_NAME_VISIBLE_OBJECT
+            = ReflectUtil.getFieldValue(Entity.class, "aA");
+    private static final DataWatcherObject<Boolean> ENTITY_HAS_NO_GRAVITY_OBJECT
+            = ReflectUtil.getFieldValue(Entity.class, "aB");
+    private static final DataWatcherObject<Byte> ARMOR_STAND_PROPERTIES_OBJECT
+            = ReflectUtil.getFieldValue(EntityArmorStand.class, "a");
+    private static final DataWatcherObject<Optional<ItemStack>> ITEM_STACK_OBJECT
+            = ReflectUtil.getFieldValue(EntityItem.class, "c");
+
+    static final EntityMetadataType<Byte> ENTITY_PROPERTIES = new EntityMetadataType<>(ENTITY_PROPERTIES_OBJECT);
+    static final EntityMetadataType<String> ENTITY_CUSTOM_NAME = new EntityMetadataType<>(ENTITY_CUSTOM_NAME_OBJECT);
+    static final EntityMetadataType<Boolean> ENTITY_CUSTOM_NAME_VISIBLE = new EntityMetadataType<>(ENTITY_CUSTOM_NAME_VISIBLE_OBJECT);
+    static final EntityMetadataType<Boolean> ENTITY_HAS_NO_GRAVITY = new EntityMetadataType<>(ENTITY_HAS_NO_GRAVITY_OBJECT);
+    static final EntityMetadataType<Byte> ARMOR_STAND_PROPERTIES = new EntityMetadataType<>(ARMOR_STAND_PROPERTIES_OBJECT);
+    static final EntityMetadataType<Optional<ItemStack>> ITEM_STACK = new EntityMetadataType<>(ITEM_STACK_OBJECT);
+
+    private final DataWatcherObject<T> dataWatcherObject;
+
+    private EntityMetadataType(DataWatcherObject<T> dataWatcherObject) {
+        this.dataWatcherObject = dataWatcherObject;
+    }
+
+    DataWatcher.Item<T> construct(T value) {
+        return new DataWatcher.Item<>(dataWatcherObject, value);
+    }
+
+}
