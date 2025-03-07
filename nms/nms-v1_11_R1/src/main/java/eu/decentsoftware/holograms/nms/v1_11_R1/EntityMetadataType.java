594a0353ed89195e97b3dff810705bbc3d6b16b7
d0by1 <dobbyvoe@gmail.com>
2025-03-08 00:30:41 +0100

    Implement NMS for v1_11_R1

---
+++ nms/nms-v1_11_R1/src/main/java/eu/decentsoftware/holograms/nms/v1_11_R1/EntityMetadataType.java
@@ -0,0 +1,46 @@
+package eu.decentsoftware.holograms.nms.v1_11_R1;
+
+import eu.decentsoftware.holograms.shared.reflect.ReflectUtil;
+import net.minecraft.server.v1_11_R1.DataWatcher;
+import net.minecraft.server.v1_11_R1.DataWatcherObject;
+import net.minecraft.server.v1_11_R1.Entity;
+import net.minecraft.server.v1_11_R1.EntityArmorStand;
+import net.minecraft.server.v1_11_R1.EntityItem;
+import net.minecraft.server.v1_11_R1.ItemStack;
+
+class EntityMetadataType<T> {
+
+    private static final DataWatcherObject<Byte> ENTITY_PROPERTIES_OBJECT
+            = ReflectUtil.getFieldValue(Entity.class, "Z");
+    private static final DataWatcherObject<String> ENTITY_CUSTOM_NAME_OBJECT
+            = ReflectUtil.getFieldValue(Entity.class, "aA");
+    private static final DataWatcherObject<Boolean> ENTITY_CUSTOM_NAME_VISIBLE_OBJECT
+            = ReflectUtil.getFieldValue(Entity.class, "aB");
+    private static final DataWatcherObject<Boolean> ENTITY_SILENT_OBJECT
+            = ReflectUtil.getFieldValue(Entity.class, "aC");
+    private static final DataWatcherObject<Boolean> ENTITY_HAS_NO_GRAVITY_OBJECT
+            = ReflectUtil.getFieldValue(Entity.class, "aD");
+    private static final DataWatcherObject<Byte> ARMOR_STAND_PROPERTIES_OBJECT
+            = ReflectUtil.getFieldValue(EntityArmorStand.class, "a");
+    private static final DataWatcherObject<ItemStack> ITEM_STACK_OBJECT
+            = ReflectUtil.getFieldValue(EntityItem.class, "c");
+
+    static final EntityMetadataType<Byte> ENTITY_PROPERTIES = new EntityMetadataType<>(ENTITY_PROPERTIES_OBJECT);
+    static final EntityMetadataType<String> ENTITY_CUSTOM_NAME = new EntityMetadataType<>(ENTITY_CUSTOM_NAME_OBJECT);
+    static final EntityMetadataType<Boolean> ENTITY_CUSTOM_NAME_VISIBLE = new EntityMetadataType<>(ENTITY_CUSTOM_NAME_VISIBLE_OBJECT);
+    static final EntityMetadataType<Boolean> ENTITY_SILENT = new EntityMetadataType<>(ENTITY_SILENT_OBJECT);
+    static final EntityMetadataType<Boolean> ENTITY_HAS_NO_GRAVITY = new EntityMetadataType<>(ENTITY_HAS_NO_GRAVITY_OBJECT);
+    static final EntityMetadataType<Byte> ARMOR_STAND_PROPERTIES = new EntityMetadataType<>(ARMOR_STAND_PROPERTIES_OBJECT);
+    static final EntityMetadataType<ItemStack> ITEM_STACK = new EntityMetadataType<>(ITEM_STACK_OBJECT);
+
+    private final DataWatcherObject<T> dataWatcherObject;
+
+    private EntityMetadataType(DataWatcherObject<T> dataWatcherObject) {
+        this.dataWatcherObject = dataWatcherObject;
+    }
+
+    DataWatcher.Item<T> construct(T value) {
+        return new DataWatcher.Item<>(dataWatcherObject, value);
+    }
+
+}
