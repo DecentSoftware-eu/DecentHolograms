d226d6c309aa0bdd211cf4530aa74240c5d98566
d0by1 <dobbyvoe@gmail.com>
2025-03-08 22:07:34 +0100

    Implement NMS for v1_16_R3

---
+++ nms/nms-v1_16_R3/src/main/java/eu/decentsoftware/holograms/nms/v1_16_R3/IconHologramRenderer.java
@@ -0,0 +1,74 @@
+package eu.decentsoftware.holograms.nms.v1_16_R3;
+
+import eu.decentsoftware.holograms.nms.api.renderer.NmsIconHologramRenderer;
+import eu.decentsoftware.holograms.shared.DecentPosition;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+class IconHologramRenderer implements NmsIconHologramRenderer {
+
+    private final int itemEntityId;
+    private final int armorStandEntityId;
+
+    IconHologramRenderer(EntityIdGenerator entityIdGenerator) {
+        this.itemEntityId = entityIdGenerator.getFreeEntityId();
+        this.armorStandEntityId = entityIdGenerator.getFreeEntityId();
+    }
+
+    @Override
+    public void display(Player player, DecentPosition position, ItemStack content) {
+        EntityPacketsBuilder.create()
+                .withSpawnEntityLiving(armorStandEntityId, EntityType.ARMOR_STAND, offsetPosition(position))
+                .withEntityMetadata(armorStandEntityId, EntityMetadataBuilder.create()
+                        .withInvisible()
+                        .withArmorStandProperties(true, true)
+                        .toWatchableObjects())
+                .withSpawnEntity(itemEntityId, EntityType.DROPPED_ITEM, position)
+                .withEntityMetadata(itemEntityId, EntityMetadataBuilder.create()
+                        .withItemStack(content)
+                        .toWatchableObjects())
+                .withTeleportEntity(itemEntityId, position)
+                .withPassenger(armorStandEntityId, itemEntityId)
+                .sendTo(player);
+    }
+
+    @Override
+    public void updateContent(Player player, DecentPosition position, ItemStack content) {
+        EntityPacketsBuilder.create()
+                .withEntityMetadata(itemEntityId, EntityMetadataBuilder.create()
+                        .withItemStack(content)
+                        .toWatchableObjects())
+                .sendTo(player);
+    }
+
+    @Override
+    public void move(Player player, DecentPosition position) {
+        EntityPacketsBuilder.create()
+                .withTeleportEntity(armorStandEntityId, offsetPosition(position))
+                .sendTo(player);
+    }
+
+    @Override
+    public void hide(Player player) {
+        EntityPacketsBuilder.create()
+                .withRemovePassenger(armorStandEntityId)
+                .withRemoveEntity(itemEntityId)
+                .withRemoveEntity(armorStandEntityId)
+                .sendTo(player);
+    }
+
+    @Override
+    public double getHeight(ItemStack content) {
+        return 0.5d;
+    }
+
+    @Override
+    public int[] getEntityIds() {
+        return new int[]{armorStandEntityId, itemEntityId};
+    }
+
+    private DecentPosition offsetPosition(DecentPosition position) {
+        return position.subtractY(0.55);
+    }
+}
