39007654b893eae04f4cbcea45f3ace97c5237f1
d0by1 <dobbyvoe@gmail.com>
2025-03-08 21:49:57 +0100

    Implement NMS for v1_16_R1

---
+++ nms/nms-v1_16_R1/src/main/java/eu/decentsoftware/holograms/nms/v1_16_R1/EntityIdGenerator.java
@@ -0,0 +1,26 @@
+package eu.decentsoftware.holograms.nms.v1_16_R1;
+
+import eu.decentsoftware.holograms.nms.api.DecentHologramsNmsException;
+import eu.decentsoftware.holograms.shared.reflect.ReflectField;
+import net.minecraft.server.v1_16_R1.Entity;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+class EntityIdGenerator {
+
+    private static final ReflectField<AtomicInteger> ENTITY_COUNT_FIELD = new ReflectField<>(Entity.class, "entityCount");
+
+    int getFreeEntityId() {
+        try {
+            /*
+             * We are getting the new entity ids the same way as the server does. This is to ensure
+             * that the ids are unique and don't conflict with any other entities.
+             */
+            AtomicInteger entityCount = ENTITY_COUNT_FIELD.get(null);
+            return entityCount.incrementAndGet();
+        } catch (Exception e) {
+            throw new DecentHologramsNmsException("Failed to get new entity ID", e);
+        }
+    }
+
+}
