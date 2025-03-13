d73fd35bbf21f60f7e22147004c3e7b926f84b92
d0by1 <dobbyvoe@gmail.com>
2025-03-13 09:29:14 +0100

    Implement NMS for v1_20_R2

---
+++ nms/nms-v1_20_R2/src/main/java/eu/decentsoftware/holograms/nms/v1_20_R2/SmallHeadHologramRenderer.java
@@ -0,0 +1,11 @@
+package eu.decentsoftware.holograms.nms.v1_20_R2;
+
+import eu.decentsoftware.holograms.nms.api.renderer.NmsSmallHeadHologramRenderer;
+
+class SmallHeadHologramRenderer extends HeadHologramRenderer implements NmsSmallHeadHologramRenderer {
+
+    SmallHeadHologramRenderer(EntityIdGenerator entityIdGenerator) {
+        super(entityIdGenerator, true);
+    }
+
+}
