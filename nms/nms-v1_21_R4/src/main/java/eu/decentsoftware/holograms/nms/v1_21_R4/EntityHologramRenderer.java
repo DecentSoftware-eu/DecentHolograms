8218eddbf97321c2ece5f9dbd98256b41f67d679
d0by1 <dobbyvoe@gmail.com>
2025-03-27 22:20:15 +0100

    Implement NMS for v1_21_R4

---
+++ nms/nms-v1_21_R4/src/main/java/eu/decentsoftware/holograms/nms/v1_21_R4/EntityHologramRenderer.java
@@ -0,0 +1,61 @@
+package eu.decentsoftware.holograms.nms.v1_21_R4;
+
+import eu.decentsoftware.holograms.nms.api.renderer.NmsEntityHologramRenderer;
+import eu.decentsoftware.holograms.shared.DecentPosition;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+class EntityHologramRenderer implements NmsEntityHologramRenderer {
+
+    private final int entityId;
+
+    EntityHologramRenderer(EntityIdGenerator entityIdGenerator) {
+        this.entityId = entityIdGenerator.getFreeEntityId();
+    }
+
+    @Override
+    public void display(Player player, DecentPosition position, EntityType content) {
+        DecentPosition offsetPosition = offsetPosition(position);
+        EntityPacketsBuilder.create()
+                .withSpawnEntity(entityId, content, offsetPosition)
+                .withEntityMetadata(entityId, EntityMetadataBuilder.create()
+                        .withSilent()
+                        .withNoGravity()
+                        .toWatchableObjects())
+                .sendTo(player);
+    }
+
+    @Override
+    public void updateContent(Player player, DecentPosition position, EntityType content) {
+        hide(player);
+        display(player, position, content);
+    }
+
+    @Override
+    public void move(Player player, DecentPosition position) {
+        EntityPacketsBuilder.create()
+                .withTeleportEntity(entityId, offsetPosition(position))
+                .sendTo(player);
+    }
+
+    @Override
+    public void hide(Player player) {
+        EntityPacketsBuilder.create()
+                .withRemoveEntity(entityId)
+                .sendTo(player);
+    }
+
+    @Override
+    public double getHeight(EntityType content) {
+        return EntityTypeRegistry.getEntityTypeHeight(content);
+    }
+
+    @Override
+    public int[] getEntityIds() {
+        return new int[]{entityId};
+    }
+
+    private DecentPosition offsetPosition(DecentPosition position) {
+        return position.subtractY(0.25d);
+    }
+}
