b3d446f596bf2ecf2200a014b21d5e437f14c8fe
d0by1 <dobbyvoe@gmail.com>
2025-06-15 18:33:17 +0200

    Prepare lines

---
+++ api/src/main/java/eu/decentsoftware/holograms/api/hologram/line/HeadHologramLine.java
@@ -0,0 +1,69 @@
+/*
+ * This file is part of DecentHolograms, licensed under the GNU GPL v3.0 License.
+ * Copyright (C) DecentSoftware.eu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package eu.decentsoftware.holograms.api.hologram.line;
+
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public interface HeadHologramLine extends HologramLine {
+
+    /**
+     * Get the {@link ItemStack} of this line. This is the item that will be displayed
+     * as a head in the hologram.
+     *
+     * @return The {@link ItemStack} of this line.
+     * @since 2.10.0
+     */
+    @NotNull
+    ItemStack getItemStack();
+
+    /**
+     * Set the {@link ItemStack} of this line. This is the item that will be displayed
+     * as a head in the hologram.
+     * <p>
+     * This method also updates the line accordingly.
+     *
+     * @param itemStack The {@link ItemStack} of this line.
+     * @since 2.10.0
+     */
+    void setItemStack(@NotNull ItemStack itemStack);
+
+    /**
+     * Get the facing direction of the head. This is the angle in degrees
+     * that the head is facing.
+     * <p>
+     * This value overrides the default facing direction of the hologram.
+     *
+     * @return The facing direction of the head in degrees.
+     * @since 2.10.0
+     */
+    float getFacing();
+
+    /**
+     * Set the facing direction of the head. This is the angle in degrees
+     * that the head is facing.
+     * <p>
+     * This value overrides the default facing direction of the hologram.
+     *
+     * @param facing The facing direction of the head in degrees. Must be between 0 and 360.
+     * @since 2.10.0
+     */
+    void setFacing(float facing);
+
+}
