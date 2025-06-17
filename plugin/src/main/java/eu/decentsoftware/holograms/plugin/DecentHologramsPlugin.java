28e44809d2df1b9b4c05b6d150a8ef58eb5ba70b
d0by1 <dobbyvoe@gmail.com>
2025-06-18 01:16:46 +0200

    Prevent runtime errors from nbt api

--- plugin/src/main/java/eu/decentsoftware/holograms/plugin/DecentHologramsPlugin.java
+++ plugin/src/main/java/eu/decentsoftware/holograms/plugin/DecentHologramsPlugin.java
@@ -1,11 +1,11 @@
 package eu.decentsoftware.holograms.plugin;
 
-import de.tr7zw.changeme.nbtapi.NBT;
 import eu.decentsoftware.holograms.api.DecentHolograms;
 import eu.decentsoftware.holograms.api.DecentHologramsAPI;
 import eu.decentsoftware.holograms.api.commands.CommandManager;
 import eu.decentsoftware.holograms.api.commands.DecentCommand;
 import eu.decentsoftware.holograms.api.utils.reflect.Version;
+import eu.decentsoftware.holograms.hook.NbtApiHook;
 import eu.decentsoftware.holograms.plugin.commands.HologramsCommand;
 import eu.decentsoftware.holograms.plugin.features.DamageDisplayFeature;
 import eu.decentsoftware.holograms.plugin.features.HealingDisplayFeature;
@@ -14,49 +14,49 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class DecentHologramsPlugin extends JavaPlugin {
 
-	private boolean unsupportedServerVersion = false;
+    private boolean unsupportedServerVersion = false;
 
-	@Override
-	public void onLoad() {
-		if (Version.CURRENT == null) {
-			unsupportedServerVersion = true;
-			return;
-		}
+    @Override
+    public void onLoad() {
+        if (Version.CURRENT == null) {
+            unsupportedServerVersion = true;
+            return;
+        }
 
-		DecentHologramsAPI.onLoad(this);
-	}
+        DecentHologramsAPI.onLoad(this);
+    }
 
-	@Override
-	public void onEnable() {
-		if (unsupportedServerVersion) {
-			getLogger().severe("Unsupported server version detected: " + Bukkit.getServer().getVersion());
-			getLogger().severe("Plugin will now be disabled.");
-			Bukkit.getPluginManager().disablePlugin(this);
-			return;
-		}
+    @Override
+    public void onEnable() {
+        if (unsupportedServerVersion) {
+            getLogger().severe("Unsupported server version detected: " + Bukkit.getServer().getVersion());
+            getLogger().severe("Plugin will now be disabled.");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
 
-		DecentHologramsAPI.onEnable();
+        DecentHologramsAPI.onEnable();
 
-		DecentHolograms decentHolograms = DecentHologramsAPI.get();
-		decentHolograms.getFeatureManager().registerFeature(new DamageDisplayFeature());
-		decentHolograms.getFeatureManager().registerFeature(new HealingDisplayFeature());
+        DecentHolograms decentHolograms = DecentHologramsAPI.get();
+        decentHolograms.getFeatureManager().registerFeature(new DamageDisplayFeature());
+        decentHolograms.getFeatureManager().registerFeature(new HealingDisplayFeature());
 
-		CommandManager commandManager = decentHolograms.getCommandManager();
-		DecentCommand mainCommand = new HologramsCommand();
-		commandManager.setMainCommand(mainCommand);
-		commandManager.registerCommand(mainCommand);
-		
-		// Enable NBT API to avoid lag spikes when parsing NBT for the first time.
-		NBT.preloadApi();
-	}
+        CommandManager commandManager = decentHolograms.getCommandManager();
+        DecentCommand mainCommand = new HologramsCommand();
+        commandManager.setMainCommand(mainCommand);
+        commandManager.registerCommand(mainCommand);
 
-	@Override
-	public void onDisable() {
-		if (unsupportedServerVersion) {
-			return;
-		}
+        // Enable NBT API to avoid lag spikes when parsing NBT for the first time.
+        NbtApiHook.initialize();
+    }
 
-		DecentHologramsAPI.onDisable();
-	}
+    @Override
+    public void onDisable() {
+        if (unsupportedServerVersion) {
+            return;
+        }
+
+        DecentHologramsAPI.onDisable();
+    }
 
 }
